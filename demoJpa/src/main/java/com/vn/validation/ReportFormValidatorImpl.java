a9380cac9f9cc95f87f98e17deccbef01d5558cc
duyetnguyendinh19 <46662469+duyetnguyendinh19@users.noreply.github.com>
2019-10-15 09:04:55 +0700

    cm report

---
+++ demoJpa/src/main/java/com/vn/validation/ReportFormValidatorImpl.java
@@ -0,0 +1,57 @@
+package com.vn.validation;
+
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.vn.jpa.Report;
+import com.vn.validation.service.ReportFormValidator;
+
+@Service(value="reportFormValidator")
+public class ReportFormValidatorImpl implements Validator, ReportFormValidator {
+
+	@Override
+	public void validateReportForm(Object var1, Errors var2) {
+		this.validate(var1, var2);
+	}
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Report.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Report report = (Report) target;
+		if (this.isBlank(report.getName())) {
+			errors.reject("name", "Không được để trống tên !");
+		}
+		if (this.isBlank(report.getMobile())) {
+			errors.reject("mobile", "Không được để trống số điện thoại !");
+		}
+		if (this.isBlank(report.getProblem())) {
+			errors.reject("problem", "Không được để trống vấn đề !");
+		}
+		if (this.isBlank(report.getOpinion())) {
+			errors.reject("opinion", "Không được để trống ý kiến !");
+		}
+		this.emailValidate(report.getEmail(),errors);
+	}
+
+	private boolean isBlank(String string) {
+		return string == null || string.trim().isEmpty();
+	}
+
+	private void emailValidate(String email, Errors errors) {
+		String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+		Pattern pattern = Pattern.compile(emailPattern);
+		if (this.isBlank(email)) {
+			errors.reject("email", "Không được để trống Email !");
+		} else if (!pattern.matcher(email.trim()).matches()) {
+			errors.reject("email", "Email không đúng định dạng !");
+		}
+	}
+
+}
---
+++ demoJpa/src/main/java/com/vn/validation/service/ReportFormValidator.java
@@ -0,0 +1,7 @@
+package com.vn.validation.service;
+
+import org.springframework.validation.Errors;
+
+public interface ReportFormValidator {
+	 void validateReportForm(Object var1, Errors var2);
+}
