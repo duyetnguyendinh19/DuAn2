c5e94b5c959ec298a6d2a62647bb0776456add5b
Bùi Tấn <tanbv.dev@gmail.com>
2019-10-16 19:14:36 +0700

    update code

--- demoJpa/src/main/java/com/vn/jpa/BankInfo.java
+++ demoJpa/src/main/java/com/vn/jpa/BankInfo.java
@@ -26,13 +26,37 @@ public class BankInfo implements Serializable {
     @Column(name = "bank_icon")
     private String icon;
 
+    @Column(name = "bank_type")
+    private int type;
+
+    @Column(name = "bank_info", columnDefinition = "TEXT")
+    private String info;
+
     public BankInfo() {
     }
 
-    public BankInfo(String code, String name, String icon) {
+    public BankInfo(String code, String name, String icon, int type, String info) {
         this.code = code;
         this.name = name;
         this.icon = icon;
+        this.type = type;
+        this.info = info;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
     }
 
     public Long getId() {
