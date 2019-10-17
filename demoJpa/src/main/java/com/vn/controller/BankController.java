3b00180a3c916f45422caeec6430595f5a5f7fc5
Bùi Tấn <tanbv.dev@gmail.com>
2019-10-17 10:35:40 +0700

    update code

--- demoJpa/src/main/java/com/vn/controller/BankController.java
+++ demoJpa/src/main/java/com/vn/controller/BankController.java
@@ -3,6 +3,7 @@ package com.vn.controller;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.vn.jpa.BankInfo;
+import com.vn.model.BankModel;
 import com.vn.service.BankInfoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -22,12 +23,34 @@ public class BankController {
     public @ResponseBody String listBank(@RequestParam("type") Integer type){
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         List<BankInfo> lsInfo = new ArrayList<>() ;
+        List<BankModel> lsmodel = new ArrayList<>();
         try {
             lsInfo = bankInfoService.findAllByType(type);
+            for(BankInfo x : lsInfo){
+                BankModel model = new BankModel();
+                model.setId(x.getId());
+                model.setIcon(x.getIcon());
+                model.setCode(x.getCode());
+                model.setName(x.getName());
+                lsmodel.add(model);
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
-        return gson.toJson(lsInfo);
+        return gson.toJson(lsmodel);
+    }
+
+    @RequestMapping(value = "findOne.html", method = RequestMethod.GET)
+    public @ResponseBody String findOne(@RequestParam("id") Long id){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        BankModel model = new BankModel();
+        try {
+            BankInfo info = bankInfoService.findOne(id);
+            model.setInfo(info.getInfo());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return gson.toJson(model);
     }
 
 }
