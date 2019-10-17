49ebd24bc64ed2365b842e0c2a2ce2e6d9e789c5
duyetnguyendinh19 <46662469+duyetnguyendinh19@users.noreply.github.com>
2019-10-17 09:56:21 +0700

    fix reload cart

--- demoJpa/src/main/java/com/vn/controller/CartController.java
+++ demoJpa/src/main/java/com/vn/controller/CartController.java
@@ -11,11 +11,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.vn.jpa.Product;
 import com.vn.model.Cart;
 import com.vn.service.ProductService;
