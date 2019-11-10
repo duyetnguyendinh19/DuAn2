fbd3e12e782ae6878609eaa86a2e9a56ab8da01c
duyetnguyendinh19 <46662469+duyetnguyendinh19@users.noreply.github.com>
2019-11-10 18:05:42 +0700

    cm form bill Cancel

--- demoJpa/src/main/java/com/vn/controller/BillController.java
+++ demoJpa/src/main/java/com/vn/controller/BillController.java
@@ -45,7 +45,7 @@ public class BillController {
     private String DELETE = "N";
 
     @RequestMapping(value = "list.html", method = {RequestMethod.GET, RequestMethod.POST})
-    @PreAuthorize("hasAnyAuthority('Administrators','Staffs')")
+//    @PreAuthorize("hasAnyAuthority('Administrators','Staffs')")
     public String list(Model model, HttpSession session, HttpServletRequest request, Pageable pageable,
                        @RequestParam(value = "from_date", defaultValue = "") String fromDate,
                        @RequestParam(value = "to_date", defaultValue = "") String toDate,
@@ -96,6 +96,56 @@ public class BillController {
         }
         return "admin/bill/list";
     }
+    
+    @RequestMapping(value = "listCancel.html", method = {RequestMethod.GET, RequestMethod.POST})
+//  @PreAuthorize("hasAnyAuthority('Administrators','Staffs')")
+  public String listCancel(Model model, HttpSession session, HttpServletRequest request, Pageable pageable,
+                     @RequestParam(value = "from_date", defaultValue = "") String fromDate,
+                     @RequestParam(value = "to_date", defaultValue = "") String toDate,
+                     @RequestParam(value = "code", defaultValue = "") String code
+                   ) {
+      try {
+          DateTime time = new DateTime();
+          Date _fromDate = time.withTimeAtStartOfDay().toDate();
+          Date _toDate = time.withTime(23, 59, 59, 999).toDate();
+
+          if (Strings.isNullOrEmpty(fromDate) && request.getMethod().equalsIgnoreCase("GET")) {
+              fromDate = (String) session.getAttribute("from_date");
+              if (Strings.isNullOrEmpty(fromDate)) {
+                  fromDate = sdf.format(_fromDate);
+              }
+          }
+          if (Strings.isNullOrEmpty(toDate) && request.getMethod().equalsIgnoreCase("GET")) {
+              toDate = (String) session.getAttribute("to_date");
+              if (Strings.isNullOrEmpty(toDate)) {
+                  toDate = sdf.format(_toDate);
+              }
+          }
+          if (Strings.isNullOrEmpty(code) && request.getMethod().equalsIgnoreCase("GET")) {
+              code = (String) session.getAttribute("code");
+          }
+          
+          try {
+              _fromDate = sdf.parse(fromDate);
+              _toDate = sdf.parse(toDate);
+          } catch (ParseException e) {
+              e.printStackTrace();
+          }
+          model.addAttribute("from_date", fromDate);
+          model.addAttribute("to_date", toDate);
+          model.addAttribute("code", code);
+
+          Sort sort = new Sort(Sort.Direction.DESC, "id");
+          Pageable _pageable = new PageRequest(pageable.getPageNumber(), Constants.Paging.SIZE, sort);
+          Page<Bill> page = billService.findAllBill(_fromDate, _toDate, 0, code.trim(), DELETE, _pageable);
+          model.addAttribute("page", page);
+          session.setAttribute("from_date", sdf.format(_fromDate));
+          session.setAttribute("to_date", sdf.format(_toDate));
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      return "admin/bill/listCancel";
+  }
 
     @RequestMapping(value = "accept/{id}/payment.html", method = RequestMethod.GET)
     @PreAuthorize("hasAnyAuthority('Administrators','Staffs')")
